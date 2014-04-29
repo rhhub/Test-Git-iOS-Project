9524715e13307985d675354a2edc6c16dfd41b98
rhhub <ryanhuebert@gmail.com>
2014-04-29 16:56:09 -0700

    first commit

---
+++ Git Test/RHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RHAppDelegate.h
+//  Git Test
+//
+//  Created by VoidMac on 4/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by VoidMac on 4/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RHAppDelegate class]));
+    }
+}
