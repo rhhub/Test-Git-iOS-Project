61ea1810ebf406853c2153c4b8ca1026732c4905
rhhub <ryanhuebert@gmail.com>
2014-04-29 17:03:37 -0700

    added test class

---
+++ RHTest.h
@@ -0,0 +1,13 @@
+//
+//  RHTest.h
+//  Git Test
+//
+//  Created by VoidMac on 4/29/14.
+//  Copyright (c) 2014 VoidMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RHTest : NSObject
+
+@end
---
+++ RHTest.m
@@ -0,0 +1,13 @@
+//
+//  RHTest.m
+//  Git Test
+//
+//  Created by VoidMac on 4/29/14.
+//  Copyright (c) 2014 VoidMac. All rights reserved.
+//
+
+#import "RHTest.h"
+
+@implementation RHTest
+
+@end
